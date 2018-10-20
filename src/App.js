ce21afd6cd10b9e3387cd6d93881ee09a91d7da8
Sara Blevins <dvlnthebludrs@gmail.com>
2018-10-20 18:14:37 -0400

    Move header to its own component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-
-// import logo from './logo.svg';
 import Footer from './Footer';
 import './App.css';
+import Header from './Header';
 import MainContent from './MainContent';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="App-header">
-					<h2>Kennedy Space Center Neighborhood Fun Finder!</h2>
-				</div>
+				<Header />
 				<MainContent />
 				<Footer />
 			</div>
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+
+    render() {
+        return <div className="App-header">
+            <h2>Kennedy Space Center Neighborhood Fun Finder!</h2>
+        </div>
+    }
+}
