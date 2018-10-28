14ead9e1c5aabc24e0ae9f7f7b497702fd8ac7f3
Sara Blevins <dvlnthebludrs@gmail.com>
2018-10-28 16:53:33 -0400

    Create venues in SideBar

--- src/SideBar.js
+++ src/SideBar.js
@@ -6,18 +6,35 @@ export default class SideBar extends Component {
     render() {
 
         const listVenues = [
-            { name: "mylist", capital: "someword" },
+            { name: "Apollo/Saturn V Center" },
+            { name: "Playalinda Beach"},
+            { name: "Launch Control Center"},
+            { name: "Kennedy Space Center"},
+            { name: "Launch Pad 39A (LC-39A)"},
+            { name: "Space View Park"},
+            { name: "Atlantis Exhibit Kennedy Space Center"},
+            { name: "Kennedy Space Center Visitor Complex"},
+            { name: "Rocket Garden"},
+            { name: "Press Site"},
+            { name: "Early Space Exploration"},
+            { name: "Astronaut Memorial"},
+            { name: "Launch Pad 39 Observation Gantry"},
+            { name: "Shuttle Launch Experience"},
+            { name: "Astronaut Hall Of Fame"},
+            { name: "Aquaduck, Disney Dream"},
+            { name: "Indian River"},
+            { name: "Banana River"},
+            { name: "Port Canaveral"}
         ]
 
         return (
             <div className="sideBar">
-                <ol>
+                <input type={"search"} id={"search"} placeholder={"Filter Venues"} />
+                <ul>
                     {listVenues.map(venue => (
-                        <li>
-                            Capital of {venue.name} is {venue.capital}
-                        </li>
+                        <li>{venue.name}</li>
                     ))}
-                </ol>
+                </ul>
             </div>
         );
     }
